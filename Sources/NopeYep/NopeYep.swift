7a0fba3c16315cf21150fc60b4722e2310225787
Andrew Bennet <AndrewBennet@users.noreply.github.com>
2021-03-03 16:59:16 +0000

    Include common boolean-emoji expressions

--- Sources/NopeYep/NopeYep.swift
+++ Sources/NopeYep/NopeYep.swift
@@ -3,9 +3,9 @@ import Foundation
 extension Bool: ExpressibleByStringLiteral {
     public init(stringLiteral value: String) {
         switch value.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) {
-        case "nope","","0","nan","👎","👎🏻","👎🏼","👎🏽","👎🏾","👎🏿", "no yeah":
+        case "nope","","0","nan","👎","👎🏻","👎🏼","👎🏽","👎🏾","👎🏿","❌","no yeah":
             self = false
-        case "yep","1","0 but true","👍","👍🏻","👍🏼","👍🏽","👍🏾","👍🏿", "yeah no", "for sure":
+        case "yep","1","0 but true","👍","👍🏻","👍🏼","👍🏽","👍🏾","👍🏿","✔️","yeah no", "for sure":
             self = true
         case "maybe", "🤷🏼‍♂️":
             self = Bool.random()
